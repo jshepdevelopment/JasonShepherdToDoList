18d5af35f6d97b4793bf072677b1d3a6142a76d1
Mac <mac@macbook.local>
2016-03-04 13:18:05 -0700

    Initial Commit

---
+++ JasonShepherdToDoList/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  JasonShepherdToDoList
+//
+//  Created by Mac on 3/4/16.
+//  Copyright © 2016 Salt Lake Community College. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ JasonShepherdToDoList/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  JasonShepherdToDoList
+//
+//  Created by Mac on 3/4/16.
+//  Copyright © 2016 Salt Lake Community College. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
