6f5125e04cdd1c893d79c016a2edcf09c56de65d
Mac <mac@macbook.local>
2016-03-04 14:01:08 -0700

    Updated

--- JasonShepherdToDoList/SecondViewController.swift
+++ JasonShepherdToDoList/SecondViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myTextField: UITextField!
+    
+   
+    @IBAction func myButton(sender: AnyObject) {
+        myLabel.text = myTextField.text
+    }
+
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.myTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,16 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // Closes the keyboard with tap outside
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    // Closes keyboard when return button pressed
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
 
